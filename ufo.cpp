7c649f18417f5d235e81e6d1645664c54046ad13
nlonghorne <77103964+nlonghorne@users.noreply.github.com>
2021-01-07 11:37:05 +0000

    Add files via upload

---
+++ ufo.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  void greet();
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  while (answer != codeword && misses < 7) {
+    display_misses(misses);
+    display_status(incorrect, answer);
+    std::cout << "\nPlease enter your guess:\n";
+    std::cin >> letter;
+    for (int k = 0; k < codeword.length(); k++) {
+      if (letter == codeword[k]) {
+        answer[k] = letter;
+        guess = true;
+      }
+    }
+    if (guess == true) {
+      std::cout << "Correct!\n";
+    }
+    else {
+      std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+    guess = false;
+  }
+ 
+  end_game(answer, codeword);
+}
